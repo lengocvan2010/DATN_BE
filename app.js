37e32bfa926d699438694c5afa62ce83aba6dd72
Van Le Ngoc <vanln@officevnpay.com>
2026-02-02 04:10:20 +0700

    fd

--- app.js
+++ app.js
@@ -16,10 +16,27 @@ var app = express();
 connectDB();
 
 // Middleware
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://datn-fe-silk.vercel.app'
+];
+
 app.use(cors({
-  origin: 'http://localhost:3000', // FE Next
-  credentials: true               // ⭐ BẮT BUỘC để gửi cookie
+  origin: function (origin, callback) {
+    // Cho phép Postman / server-to-server
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+
+    return callback(new Error('Not allowed by CORS'));
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
 
 app.use(logger('dev'));
 
